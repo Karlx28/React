06bdc0ae9790f3b1777ccc486ab08281a01a8309
Karlx28 <mo0436032023@unab.edu.sv>
2025-03-06 15:01:55 -0600

    Guia 3

---
+++ Temporal/Program.cs
@@ -0,0 +1,14 @@
+﻿using reactBackend.Repository;
+
+AlumnoDAO alumnoDAO = new reactBackend.Repository.AlumnoDAO();
+var alumno = alumnoDAO.SelectAll();
+foreach (var item in alumno)
+{
+    Console.WriteLine(item.Nombre);
+}
+
+Console.WriteLine(" ");
+#region SelectById
+var selectById = alumnoDAO.GetById(1000);
+Console.WriteLine(selectById?.Nombre);
+#endregion
---
+++ reactBackend/Repository/AlumnoDAO.cs
@@ -0,0 +1,36 @@
+﻿using reactBackend.Context;
+using reactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactBackend.Repository
+{
+    public class AlumnoDAO
+    {
+        #region Contex
+
+        public RegistroAlumnoContext contexto =new RegistroAlumnoContext();
+        #endregion
+
+        #region Select All
+
+        public List<Alumno> SelectAll()
+        {
+            var alumno = contexto.Alumnos.ToList<Alumno>();
+            return alumno;
+        }
+        #endregion
+
+        #region Seleccionamos por ID
+        public Alumno? GetById (int id)
+        {
+            var alumno = contexto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+            return alumno == null ? null : alumno;
+        }
+
+        #endregion
+    }
+}
